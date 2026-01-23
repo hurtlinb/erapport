5fb6ef5ebf15977438d8cee25f93ff781b73be46
Blaise <119675179+hurtlinb@users.noreply.github.com>
2026-01-23 16:13:34 +0100

    Reorder report template fields

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2835,16 +2835,6 @@ ${teacherDisplayName}
                 />
               </label>
               <label>
-                Enseignant
-                <input
-                  type="text"
-                  value={draft.teacher || teacherName}
-                  readOnly
-                  disabled
-                  placeholder="Enseignant défini dans le modèle"
-                />
-              </label>
-              <label>
                 Date d'évaluation
                 <input
                   type="date"
@@ -2863,15 +2853,6 @@ ${teacherDisplayName}
                 />
               </label>
               <label>
-                Module
-                <input
-                  type="text"
-                  value={buildModuleLabel(draft.moduleNumber, draft.moduleTitle)}
-                  readOnly
-                  disabled
-                />
-              </label>
-              <label>
                 Compétence opérationnelle
                 <input
                   type="text"
@@ -2879,6 +2860,25 @@ ${teacherDisplayName}
                   readOnly
                   disabled
                   placeholder="Définie dans le modèle"
+                />
+              </label>
+              <label>
+                Enseignant
+                <input
+                  type="text"
+                  value={draft.teacher || teacherName}
+                  readOnly
+                  disabled
+                  placeholder="Enseignant défini dans le modèle"
+                />
+              </label>
+              <label>
+                Module
+                <input
+                  type="text"
+                  value={buildModuleLabel(draft.moduleNumber, draft.moduleTitle)}
+                  readOnly
+                  disabled
                 />
               </label>
             </div>
